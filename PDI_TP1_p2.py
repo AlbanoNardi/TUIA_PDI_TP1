bc35b8cdb6daf1d6ab1f09229a70dc206ca451e2
AlbanoNardi <79172415+AlbanoNardi@users.noreply.github.com>
2024-10-19 01:58:13 -0300

    Add files via upload
    Terminados punto a y b

--- PDI_TP1_p2.py
+++ PDI_TP1_p2.py
@@ -55,59 +55,47 @@ def umbralizar_imagen(img, thresh=128, maxval=255):
     return img, img_umbralizada
 
 # Función para obtener y dibujar contornos en la imagen
-def obtener_y_dibujar_contornos(img_umbralizada, img, grosor=1):
+def obtener_y_dibujar_contornos(img_umbralizada, grosor=1):
+
     contornos,_ = cv2.findContours(img_umbralizada, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     contornos_ordenados = sorted(contornos, key=cv2.contourArea, reverse=True)
-    
-    for i, contorno in enumerate(contornos_ordenados[:13]):  # Limitar a 13 contornos para simplificar
-        color = (255, 0, 0) if i == 0 else (0, 255, 0)  # Usamos colores diferentes
-        cv2.drawContours(img, contornos_ordenados, contourIdx=i, color=color, thickness=grosor)
-    
-    plt.imshow(img)
-    plt.show()
     
     return contornos_ordenados
 
 # Función para recortar la región de una pregunta dada su índice
 def recortar_pregunta_por_indice(index_pregunta, contornos_ordenados, img_umbralizada, dicc_indices):
-    if index_pregunta not in dicc_indices:
-        return None
     
     contorno_pregunta = contornos_ordenados[dicc_indices[index_pregunta]]
     x, y, w, h = cv2.boundingRect(contorno_pregunta)
-    roi_pregunta = img_umbralizada[y:y+h, x:x+w]  # Recortar la región de interés
-    
-    plt.imshow(roi_pregunta, cmap='gray')
-    plt.title(f'Pregunta número {index_pregunta}')
-    plt.show()
-    
-    return roi_pregunta
+    pregunta = img_umbralizada[y:y+h, x:x+w]  # Recortar la región de interés
+
+    return pregunta
 
 # Función para detectar la línea horizontal y recortar la región de respuesta
-def detectar_linea_y_recortar_respuesta(roi_pregunta):
+def detectar_linea_y_recortar_respuesta(pregunta):
     
-    contornos, _ = cv2.findContours(roi_pregunta, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contornos, _ = cv2.findContours(pregunta, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     
     contornos_ordenados = sorted(contornos, key=cv2.contourArea, reverse=True)
     
     contorno_linea = contornos_ordenados[1]  # El segundo contorno es la línea horizontal
     x, y, w, h = cv2.boundingRect(contorno_linea)
     
-    roi_respuesta = roi_pregunta[0:y, x:x+w]  # Recortamos desde la línea hacia arriba
+    respuesta = pregunta[0:y, x:x+w]  # Recortamos desde la línea hacia arriba
 
-    roi_respuesta =  roi_respuesta[::-1,] # Rotacion 180º
+    respuesta =  respuesta[::-1,] # Rotacion 180º
 
-    img_zeros = roi_respuesta==0
+    img_zeros = respuesta==0
 
     img_row_zeros = img_zeros.any(axis=1)
 
-    img_row_zeros_idxs = np.argwhere(np.logical_not(roi_respuesta.all(axis=1))) # Tengo los indices de los renglones
+    img_row_zeros_idxs = np.argwhere(np.logical_not(respuesta.all(axis=1))) # Tengo los indices de los renglones
 
     if img_row_zeros_idxs.size == 0:  # Condición si es vacío no hay respuesta
-        return 0
+        return None
     
     if img_row_zeros_idxs[0] > 10: # respuesta vacía con texto arriba
-        return 0
+        return None
 
     start_end = np.diff(img_row_zeros) # inicio y final de los textos
 
@@ -117,41 +105,37 @@ def detectar_linea_y_recortar_respuesta(roi_pregunta):
 
     end_idx = int(renglones_indxs[1])
 
-    roi_respuesta = roi_respuesta[start_idx:end_idx+1, :] # cortamos el sector respuesta
+    respuesta = respuesta[start_idx:end_idx+1, :] # cortamos el sector respuesta
 
-    roi_respuesta =  roi_respuesta[::-1,] # volvemos a rotarla
-
-    plt.imshow(roi_respuesta, cmap='gray')
-    plt.show()
+    respuesta =  respuesta[::-1,] # volvemos a rotarla
     
-    return roi_respuesta
+    return respuesta
 
 def detectar_letra(recorte_respuesta):
     # Conectar componentes
     connectivity = 8  # Conexión de 8 vecinos
-    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(recorte_respuesta, connectivity, cv2.CV_32S)
+    num_labels, _, stats, _ = cv2.connectedComponentsWithStats(recorte_respuesta, connectivity, cv2.CV_32S)
 
     # Revisar si hay más de una componente conectada (excluyendo el fondo)
     if num_labels <= 1:  # Solo hay fondo
-        return 0  # No se detectó ninguna letra
+        return None # No se detectó ninguna letra
 
     # Si hay más de una componente conectada, indicaría más de una letra o ruido
     if num_labels > 2:  # Excluyendo el fondo
-        return 0  # Hay más de una letra o ruido
+        return None  # Hay más de una letra o ruido
 
     # Obtener el rectángulo delimitador de la única letra
     x, y, w, h = stats[0][:4]  # stats[1] porque stats[0] es el fondo
 
     # Recortar la imagen de la letra desde la imagen binaria original
     letra_recortada = recorte_respuesta[y:y+h, x:x+w]  # Aquí se realiza el recorte correcto
-    plt.imshow(letra_recortada, cmap='gray')
-    plt.show()
+
     return letra_recortada  # Devolvemos la letra recortada
 
 
 def identificador_letra(letra_recortada):
     
-    img_expand = cv2.copyMakeBorder(letra_detectada, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=255) # agregamos bordes
+    img_expand = cv2.copyMakeBorder(letra_recortada, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=255) # agregamos bordes
 
     img_inv = img_expand==0 # invertimos para que quede fondo negro
 
@@ -160,12 +144,11 @@ def identificador_letra(letra_recortada):
     contours,_ = cv2.findContours (inv_uint8, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # buscamos contornos
 
     if len(contours) == 1:
-        print("C")
         return "C"
         
     if len(contours) == 3:
-        print("B")
         return "B"
+
     if len(contours) == 2:
 
         kernel = np.array([[-1, -1, -1], [2, 2, 2], [-1, -1, -1]])              # definimos un filtro horizontal para detectar líneas
@@ -183,31 +166,81 @@ def identificador_letra(letra_recortada):
         cantidad_lineas = np.sum(lineas_horizontales)
 
         if cantidad_lineas == 1:
-            print("A")
             return "A"
-    print("D")    
-    return "D"
+
+        else: 
+            return "D"
 
 # Función para recortar la parte superior de la pregunta
-def recortar_parte_superior(index_pregunta, index_pregunta_superior, contornos_ordenados, img_color, dicc_indices):
-    contorno_inf = contornos_ordenados[dicc_indices[index_pregunta]]
-    contorno_sup = contornos_ordenados[dicc_indices[index_pregunta_superior]]
+def recortar_parte_superior(contornos_ordenados, img_umbralizada, dicc_indices):
+
+    idx_pregunta_sup_izq = 1
+
+    idx_pregunta_sup_der = 6    # Ya sabemos que las pregutas superiores están en esos indices
+
+    contorno_izq = contornos_ordenados[dicc_indices[idx_pregunta_sup_izq]]
+    contorno_der = contornos_ordenados[dicc_indices[idx_pregunta_sup_der]]
     
-    _, y_inf, _, _ = cv2.boundingRect(contorno_inf)
-    _, y_sup, _, _ = cv2.boundingRect(contorno_sup)
+    _, y_inf, _, _ = cv2.boundingRect(contorno_izq)
+    _, y_sup, _, _ = cv2.boundingRect(contorno_der)
     
-    borde_inferior = min(y_inf, y_sup)
+    borde_inferior = min(y_inf, y_sup)  # buscamos la mínima altura para cortar la parte superior
     
     # Recortamos la parte superior
-    roi_superior = img_color[6:borde_inferior-6, 6:564]
+    box_superior = img_umbralizada[6:borde_inferior-6, 6:564]
     
-    plt.imshow(roi_superior)
-    plt.show()
-    
-    return roi_superior
--------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------
+    return box_superior
 
+def recortar_encabezado(idx: int, examen):
+
+    _, img_umbralizada = umbralizar_imagen(examen,thresh=140) # Subimos el thresh porque las barras inclinadas se desconectaban con 128 y las respuestas en 140 se distorcionaban
+
+    contornos_ordenados = obtener_y_dibujar_contornos(img_umbralizada)
+
+    dicc_indices = {1: 6, 2: 5, 3: 4, 4: 3, 5: 12, 6: 7, 7: 11, 8: 8, 9: 9, 10: 10}
+
+    box_superior_umb = recortar_parte_superior(contornos_ordenados, img_umbralizada, dicc_indices)
+
+    contornos_box_sup = obtener_y_dibujar_contornos(box_superior_umb)
+
+    contorno_box = contornos_box_sup[idx] # Iterar de 1 a 3
+
+    x, y, w, _ = cv2.boundingRect(contorno_box)
+
+    dato = box_superior_umb[0:y, x:x+w]  # Recortar la región de interés
+
+    return dato
+
+
+
+def contar_caracteres(img_palabra):
+
+    img_zeros = img_palabra == 0
+
+    img_row_zeros = img_zeros.any(axis=0)
+
+    start_end = np.diff(img_row_zeros)
+
+    renglones_indxs = np.argwhere(start_end)
+
+    espacios = 0
+
+    if len(renglones_indxs) == 0:
+        
+        return 0,espacios
+
+    cantidad_letras = len(renglones_indxs)/2
+
+    if len(renglones_indxs) == 2:
+        
+        return cantidad_letras,espacios
+
+    for i in range(1,len(renglones_indxs)-1,2):
+
+        if (int(renglones_indxs[i+1]) - int(renglones_indxs[i])) > 5:
+            espacios = espacios + 1
+
+    return cantidad_letras,espacios
 
 def correccion_examen(examen):
     # Diccionario para mapear las preguntas con los índices de contornos
@@ -215,29 +248,25 @@ def correccion_examen(examen):
 
     img, img_umbralizada = umbralizar_imagen(examen)
 
-    contornos_ordenados = obtener_y_dibujar_contornos(img_umbralizada, img, grosor=1)
+    contornos_ordenados = obtener_y_dibujar_contornos(img_umbralizada, grosor=1)
 
     respuestas_correctas = {1: "C", 2: "B", 3: "A", 4: "D", 5: "B", 6: "B", 7: "A", 8: "B", 9: "D", 10: "D"}
 
     for i in range(1, 11):  # Iteramos por todas las preguntas
-
+        
         pregunta = recortar_pregunta_por_indice(i, contornos_ordenados, img_umbralizada, dicc_indices)
-
-        if pregunta is None:
-            print(f"Pregunta {i}: No se pudo recortar.")
-            continue
         
         box_respuesta = detectar_linea_y_recortar_respuesta(pregunta)
 
-        if isinstance(box_respuesta, int) and box_respuesta == 0:  # Comprobar si es el entero 0
+        if box_respuesta is None:
             print(f'Pregunta {i}: MAL')
-            continue  # Salir del bucle para esta pregunta
-
+            continue
+        
         respuesta = detectar_letra(box_respuesta)
 
-        if isinstance(respuesta, int) and respuesta == 0:  # Comprobar si es el entero 0
+        if respuesta is None:
             print(f'Pregunta {i}: MAL')
-            continue  # Salir del bucle para esta pregunta
+            continue
 
         letra_identificada = identificador_letra(respuesta)
 
@@ -247,42 +276,38 @@ def correccion_examen(examen):
             print(f'Pregunta {i}: MAL')
     return
 
+def correccion_encabezado(examen):
 
-correccion_examen(img3)
+    name = recortar_encabezado(1,examen)
 
-respuestas_correctas = {1: "C", 2: "B", 3: "A", 4: "D", 5: "B", 6: "B", 7: "A", 8: "B", 9: "D", 10: "D"}
+    date = recortar_encabezado(3,examen)
 
-img_gris, img_umbralizada = umbralizar_imagen(img3,thresh=128)
+    clas = recortar_encabezado(2,examen)
 
-contornos_ordenados = obtener_y_dibujar_contornos(img_umbralizada, img3)
+    name_car,name_esp = contar_caracteres(name)
 
-dicc_indices = {1: 6, 2: 5, 3: 4, 4: 3, 5: 12, 6: 7, 7: 11, 8: 8, 9: 9, 10: 10}
+    date_car,_ = contar_caracteres(date)
 
-pregunta = recortar_pregunta_por_indice(2, contornos_ordenados, img_umbralizada, dicc_indices)
+    clas_car,_ = contar_caracteres(clas)
 
-respuesta = detectar_linea_y_recortar_respuesta(pregunta)
+    if name_esp == 1 and name_car < 26:
+        print('Name: OK')
 
-letra_detectada = detectar_letra(respuesta)
+    else:
+        print('Name: MAL')
+        
+    if date_car == 8:
+        print('Date: OK')
 
-letra_identificada = identificador_letra(letra_detectada)
+    else:
+        print('Date: MAL')
+        
+    if clas_car == 1:
+        print('Class: OK')
 
-print(letra_identificada)
+    else:
+        print('Class: MAL')
+        
+    return
 
-
-
-
-pregunta
-plt.imshow(img_inv, cmap='gray')
-plt.show()
-    
-img_expand = cv2.copyMakeBorder(letra_detectada, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=255) # agregamos bordes
-
-img_inv = img_expand==0 # invertimos para que quede fondo negro
-
-inv_uint8 = img_inv.astype(np.uint8) # conversión para que no quede bool
-
-contours,_ = cv2.findContours (inv_uint8, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # buscamos contornos
-
-len(contours)
-
-inv_uint8.dtype
+###############################################################################################################################
